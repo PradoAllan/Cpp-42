b736d478cafcc323056d78f28908d495bb91ac31
Allan <allanprado.it@gmail.com>
2024-10-17 12:23:29 -0300

    add: Makefile for cpp and first hello world

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int	main(void)
+{
+	char	s[30];
+
+	std::cout << "Hello world!" << std::endl;
+	std::cout << "Input your name" << std::endl;
+	std::cin >> s;
+	std::cout << "Hello, [" << s << "]" << std::endl;
+
+
+	return (1);
+}
