89e72d08a4bd6e4cf74c66807968f5c7e5dfb8c2
Allan <allanprado.it@gmail.com>
2024-11-07 11:05:47 -0300

    fix: problems with the constructors

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -3,7 +3,10 @@
 int main(void)
 {
     Zombie  allan;
-    
+    Zombie *z1 = allan.newZombie("z1"); // forma possivel.
+    Zombie *z2 = Zombie::newZombie("z2"); // nao pode
+    Zombie  *z3 = allan.randomChump("adaad"); // nao pode
+
     allan.set_name("Allan lindo");
     allan.announce();
     return (1);
