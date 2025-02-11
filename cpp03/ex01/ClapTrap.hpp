d516103a5352a1d8e0f7b92ba41b3cdff48ee2d6
PradoAllan <allanprado.it@gmail.com>
2025-02-11 14:34:17 -0300

    feat: :sparkles: Add new files for cpp03/ex01

---
+++ cpp03/ex01/ClapTrap.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+# include <iostream>
+
+class ClapTrap
+{
+    private:
+        const std::string   _name;
+        int                 _hitPoints;
+        int                 _energyPoints;
+        int                 _attackDamage;
+    public:
+        ClapTrap(void); // cannonical
+        ClapTrap(const ClapTrap &src); // cannonical
+        ClapTrap(std::string name);
+        ~ClapTrap(void); // cannonical
+
+        ClapTrap &operator=(const ClapTrap &src); // cannonical
+
+        void    attack(const std::string& target);
+        void    takeDamage(unsigned int amount);
+        void    beRepaired(unsigned int amount);
+
+        // getters
+        std::string getName(void) const;
+        int         getHitPoints(void) const;
+        int         getEnergyPoints(void) const;
+        int         getAttackDamage(void) const;
+};
+
+// It will be called ClapTrap and will have the following private attributes initialized
+// to the values specified in brackets:
+// • Name, which is passed as parameter to a constructor
+// • Hit points (10), represent the health of the ClapTrap
+// • Energy points (10)
+// • Attack damage (0)
+// Add the following public member functions so the ClapTrap looks more realistic:
+// • void attack(const std::string& target);
+// • void takeDamage(unsigned int amount);
+// • void beRepaired(unsigned int amount);
