4bbc3b064b61cd4a5403148e35313501d9113aa7
Allan <allanprado.it@gmail.com>
2025-02-12 13:55:51 -0300

    feat: :sparkles: finish cpp02/ex00

--- cpp02/ex00/main.cpp
+++ cpp02/ex00/main.cpp
@@ -2,15 +2,15 @@
 
 int main(void)
 {
-    Fixed   a;
-    Fixed   b(a);
-    Fixed   c;
+    Fixed a;
+    Fixed b( a );
+    Fixed c;
 
     c = b;
 
     std::cout << a.getRawBits() << std::endl;
     std::cout << b.getRawBits() << std::endl;
     std::cout << c.getRawBits() << std::endl;
-
-    return (1);
+    
+    return 0;
 }
