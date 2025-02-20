964580363e3091cba68edb849ebfa89e702d8ded
Allan <allanprado.it@gmail.com>
2025-02-20 09:02:24 -0300

    feat: :sparkles: Update cpp04/ex02 NOT DONE

--- cpp04/ex02/Animal.hpp
+++ cpp04/ex02/Animal.hpp
@@ -7,14 +7,5 @@ class Animal
     protected:
         std::string _type;
     public:
-        Animal(void); //canonical
-        Animal(const Animal &src); //canonical
-
-        virtual ~Animal(void); //canonical
-
-        Animal &operator=(const Animal &src); //canonical
-
-        virtual void    makeSound(void) const;
-
-        std::string     getType(void) const;
+        virtual void    makeSound(void) const = 0;
 };
