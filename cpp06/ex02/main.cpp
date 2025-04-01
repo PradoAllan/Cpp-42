c9a0c0f5758c99490fb330dfd2d2cb5107f4200f
Allan <allanprado.it@gmail.com>
2025-04-01 16:45:02 -0300

    feat: :sparkles: add cpp06 ex02 DONE

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -2,20 +2,11 @@
 #include <cstdlib>
 #include <exception>
 #include <ctime>
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
 
-class Base
-{
-    private:
-
-    public:
-        virtual ~Base(void) {}
-};
-
-class A : public Base {};
-class B : public Base {};
-class C : public Base {};
-
-Base * generate(void)
+Base *generate(void)
 {
     std::srand(std::time(NULL));
     switch (std::rand() % 3)
@@ -44,54 +35,51 @@ void identify(Base* p)
         std::cout << "The Base pointer is a instance of C." << std::endl;
     else
         std::cout << "Could not identify..." << std::endl;
-    
-
-    // A *a;
-    // B *b;
-    // C *c;
-
-    // try
-    // {
-    //     a = dynamic_cast<A*>(p);
-    //     if (a)
-    //     {
-    //         std::cout << "Found!!" << std::endl;
-    //         std::cout << "The Base pointer is a instance of A." << std::endl;
-    //     }
-    //     else
-    //         std::cout << "WRONG..." << std::endl;
-    // }
-    // catch (std::exception &e)
-    // {
-    //     try
-    //     {
-    //         b = dynamic_cast<B*>(p);
-    //         std::cout << "Found!!" << std::endl;
-    //         std::cout << "The Base pointer is a instance of B." << std::endl;
-    //     }
-    //     catch (std::exception &e)
-    //     {
-    //         try
-    //         {
-    //             c = dynamic_cast<C*>(p);
-    //             std::cout << "Found!!" << std::endl;
-    //             std::cout << "The Base pointer is a instance of C." << std::endl;
-    //         }
-    //         catch (std::exception &e)
-    //         {
-    //             std::cout << "Invalid data type." << std::endl;
-    //         }
-    //     }
-    // }
 }
 
-// void identify(Base& p);
+void identify(Base& p)
+{
+    try
+    {
+        A &a = dynamic_cast<A&>(p);
+        std::cout << "Found!!" << std::endl;
+        std::cout << "The Base pointer is a instance of A." << std::endl;
+        (void)a;
+    }
+    catch (std::exception &e)
+    {
+        try
+        {
+            B &b = dynamic_cast<B&>(p);
+            std::cout << "Found!!" << std::endl;
+            std::cout << "The Base pointer is a instance of B." << std::endl;
+            (void)b;
+        }
+        catch (std::exception &e)
+        {
+            try
+            {
+                C &c = dynamic_cast<C&>(p);
+                std::cout << "Found!!" << std::endl;
+                std::cout << "The Base pointer is a instance of C." << std::endl;
+                (void)c;
+            }
+            catch (std::exception &e)
+            {
+                std::cout << "Invalid data type." << std::endl;
+            }
+        }
+    }
+}
 
 int main(void)
 {
     Base *object;
 
     object = generate();
+    std::cout << "Pointer param" << std::endl;
+    identify(object);
+    std::cout << "Reference param" << std::endl;
     identify(object);
     delete object;
     return (0);
