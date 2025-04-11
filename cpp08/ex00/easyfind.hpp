fa7c484df9f33525a900b5b05dc20a6b627755b2
PradoAllan <allanprado.it@gmail.com>
2025-04-11 11:02:33 -0300

    feat: :sparkles: add cpp08 ex00 done
    Need to check if I can make the easyfind() return a bool...

--- cpp08/ex00/easyfind.hpp
+++ cpp08/ex00/easyfind.hpp
@@ -8,8 +8,8 @@ namespace myfunction
     template <typename T>
     bool easyfind(T container, const int &value)
     {
-        std::iterator<T> it;
-        
+        typename T::iterator it;
+
         it = find(container.begin(), container.end(), value);
         if (it != container.end())
         {
@@ -18,14 +18,5 @@ namespace myfunction
         }
         std::cout << "Element not found in the container" << std::endl;
         return (false);
-        // return (find(container.begin(), container.end(), value) ? true : false );
     }
 };
-
-// std::vector<int>::iterator it;
-
-// it = find (myvector.begin(), myvector.end(), 30);
-// if (it != myvector.end())
-//   std::cout << "Element found in myvector: " << *it << '\n';
-// else
-//   std::cout << "Element not found in myvector\n";
--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -1,6 +1,7 @@
 #include "easyfind.hpp"
 #include <iostream>
 #include <vector>
+#include <list>
 
 int main(void)
 {
@@ -11,7 +12,21 @@ int main(void)
     v.push_back(5);
     v.push_back(56);
     v.push_back(10);
-    std::cout << myfunction::easyfind<std::vector<int>>(v, 42) << std::endl;
+    std::cout << "Testing with container Vector..." << std::endl;
+    std::cout << myfunction::easyfind<std::vector<int> >(v, 11) << std::endl;
+    std::cout << std::endl;
+
+
+    std::list<int> l;
+
+    l.push_back(4);
+    l.push_back(42);
+    l.push_back(5);
+    l.push_back(56);
+    l.push_back(10);
+    std::cout << "Testing with container List..." << std::endl;
+    std::cout << myfunction::easyfind<std::list<int> >(l, 42) << std::endl;
+    std::cout << std::endl;
 
     return (0);
 }
